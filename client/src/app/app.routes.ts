d0c9ec81f9f3bfdb69dd24155b242a118e3b125b
sacaaa <szekelyhidi.sandor2@gmail.com>
2025-01-25 22:11:05 +0100

    edit: project configs

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ChatRoomListComponent } from './modules/chat-room-list/pages/chat-room-list/chat-room-list.component';
+import { ChatRoomComponent } from './modules/chat-room/pages/chat-room/chat-room.component';
+import { chatRoomGuard } from './core/guards/chat-room.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'chatrooms', component: ChatRoomListComponent },
+  { path: 'chatrooms/:id', component: ChatRoomComponent, canActivate: [chatRoomGuard] },
+  { path: '**', redirectTo: 'chatrooms' },
+];

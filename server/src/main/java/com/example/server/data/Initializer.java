9e562c93266d78d5f4b64342a91df175a3a45d06
sacaaa <szekelyhidi.sandor2@gmail.com>
2025-01-28 17:08:52 +0100

    edit: backend data update

--- server/src/main/java/com/example/server/data/Initializer.java
+++ server/src/main/java/com/example/server/data/Initializer.java
@@ -22,16 +22,16 @@ public class Initializer {
     @PostConstruct
     public void init() {
         chatRoomRepository.saveAll(List.of(
-            new ChatRoom(null, "chatroom_1", null, null, null),
-            new ChatRoom(null, "chatroom_2", null, null, null),
-            new ChatRoom(null, "chatroom_3", null, null, null)
-        ));
-
-        var password = Encrypt.encryptSha256("password");
-        userRepository.saveAll(List.of(
-            new User(null, "user_1", "user1@example.com", password, null, null ,null),
-            new User(null, "user_2", "user2@example.com", password, null, null ,null),
-            new User(null, "user_3", "user3@example.com", password, null, null ,null)
+                new ChatRoom(null, "TechTalk", "Discuss the latest in tech and gadgets", null, null, null),
+                new ChatRoom(null, "BookLovers", "Share and discuss your favorite books", null, null, null),
+                new ChatRoom(null, "MovieMania", "Chat about the latest movies and TV shows", null, null, null),
+                new ChatRoom(null, "FitnessGoals", "Motivation and tips for staying fit and healthy", null, null, null),
+                new ChatRoom(null, "TravelersHub", "Explore destinations and share travel tips", null, null, null),
+                new ChatRoom(null, "GamerZone", "Talk about your favorite games and strategies", null, null, null),
+                new ChatRoom(null, "MusicVibes", "Discuss music genres, artists, and playlists", null, null, null),
+                new ChatRoom(null, "FoodiesUnite", "Share recipes and talk about delicious food", null, null, null),
+                new ChatRoom(null, "CryptoCorner", "Discuss blockchain, crypto trading, and trends", null, null, null),
+                new ChatRoom(null, "CodeCamp", "Share programming tips and discuss software development", null, null, null)
         ));
     }
 

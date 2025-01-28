e981e3b885a8a25fde387cf08967ccbd5eea1027
sacaaa <szekelyhidi.sandor2@gmail.com>
2025-01-28 17:13:43 +0100

    edit: frontend project files

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { ChatRoomListComponent } from './modules/chat-room-list/pages/chat-room-list/chat-room-list.component';
 import { ChatRoomComponent } from './modules/chat-room/pages/chat-room/chat-room.component';
 import { chatRoomGuard } from './core/guards/chat-room.guard';
+import { authGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
+import { ProfileComponent } from './modules/profile/pages/profile/profile.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'chatrooms', pathMatch: 'full' },
   { path: 'chatrooms', component: ChatRoomListComponent },
-  { path: 'chatrooms/:id', component: ChatRoomComponent, canActivate: [chatRoomGuard] },
-  { path: '**', redirectTo: 'chatrooms' },
+  { path: 'chatrooms/:id', component: ChatRoomComponent, canActivate: [authGuard, chatRoomGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' },
 ];
